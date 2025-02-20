05c15edf28731b094e9174be34e1a2527c91a856
erstudio <erstudio>
2025-02-20 17:11:35 -0600

    demo

---
+++ Db.sql
@@ -0,0 +1,22 @@
+/*
+ * ER/Studio Data Architect SQL Code Generation
+ * Project :      HR Database (logical complete).DM1
+ *
+ * Date Created : Thursday, February 20, 2025 17:11:34
+ * Target DBMS : Databricks
+ */
+
+/* 
+ * TABLE: AGY 
+ */
+
+CREATE TABLE AGY
+(
+    ID          int         NOT NULL,
+    AGYNME      string,
+    MAINCCT     string,
+    CONTRACT    string,
+    CONSTRAINT PK9 PRIMARY KEY (ID) 
+)
+;
+
