900896d778e3b371a5eb028bd3dc5dbd0eb7c39c
erstudio <erstudio>
2025-05-28 11:56:51 -0500

    alter

--- Data bricks.sql
+++ Data bricks.sql
@@ -1,37 +1,10 @@
-/*
- * ER/Studio Data Architect SQL Code Generation
- * Project :      HR Database
- *
- * Date Created : Wednesday, May 28, 2025 11:53:28
- * Target DBMS : Databricks
- */
 
-/* 
- * TABLE: EMP 
- */
+-- Standard Alter Table SQL
 
-CREATE TABLE EMP
-(
-    EMPNMB                      string              NOT NULL,
-    EMPTYP                      string              NOT NULL,
-    STTDTE                      date,
-    PHONENMB                    decimal(15, 0),
-    SLRY                        string,
-    FRSNME                      string              NOT NULL,
-    SURNAME                     string              NOT NULL,
-    MEMBER                      string              NOT NULL,
-    MANAGER                     string,
-    ADRID                       int                 NOT NULL,
-    `Performance Review Score`  int,
-    `Promotion Flag`            string,
-    `Commission Payment`        array<struct<
-        `Commission Value`  int,
-        Paid                boolean,
-        `Payment Date`      date
-    >>,
-    c1                          string,
-    c2                          string,
-    PRIMARY KEY (EMPNMB) 
-)
+ALTER TABLE EMP
+    RENAME COLUMN c1 TO Demo
 ;
 
+ALTER TABLE EMP
+    RENAME COLUMN c2 TO Demo2
+;
